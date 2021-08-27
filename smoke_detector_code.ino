413b1623a90aada2e3e7b0dafe4d92a89676920b
Abhijeet  kumar <80936610+4BH1J337@users.noreply.github.com>
2021-08-27 22:06:57 +0530

    Add files via upload

---
+++ smoke_detector_code.ino
@@ -0,0 +1,45 @@
+
+int redLed = 2;
+int greenLed = 3;
+int buzzer = 4;
+int smoke = A7;
+// Your threshold value
+int sensorThres = 350;
+
+void setup() {
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(smoke, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int analogSensor = analogRead(smoke);
+
+  Serial.print("Smoke Level : ");
+  Serial.println(analogSensor);
+  // Checks if it has reached the threshold value
+  if (analogSensor >= sensorThres)
+  {
+    digitalWrite(redLed, HIGH);
+    delay(100);
+    digitalWrite(redLed, LOW);
+    delay(100);
+    digitalWrite(greenLed, HIGH);
+    delay(100);
+    digitalWrite(greenLed, LOW);
+    delay(100);
+    digitalWrite(buzzer, HIGH);
+    delay(500);
+
+  }
+  else
+  {
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, LOW);
+    digitalWrite(buzzer, LOW);
+
+  }
+  delay(100);
+}
