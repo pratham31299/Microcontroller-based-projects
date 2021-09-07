fafcce50d345f46db1dbb0e09515af95fee820b1
Abhijeet  kumar <abhijeetkr.sci@gmail.com>
2021-09-07 21:06:25 +0530

    Add files via upload

---
+++ Smart-Home-Using-Arduino_HC05BT-module.ino
@@ -0,0 +1,44 @@
+/*************************************************************
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+
+
+ *************************************************************/
+
+#define BLYNK_USE_DIRECT_CONNECT
+
+// You could use a spare Hardware Serial on boards that have it (like Mega)
+#include <SoftwareSerial.h>
+SoftwareSerial DebugSerial(0, 1); // RX, TX
+
+#define BLYNK_PRINT DebugSerial
+
+
+#include <BlynkSimpleSerialBLE.h>
+
+char auth[] = "5gxYVBZ6JAkF2x1MwtfqdmEcqfBj0gTV";
+
+void setup()
+{
+  // Debug console
+  DebugSerial.begin(9600u);
+
+  DebugSerial.println("Waiting for connections...");
+
+  // Blynk will work through Serial
+  // 9600 is for HC-06. For HC-05 default speed is 38400
+  // Do not read or write this serial manually in your sketch
+  Serial.begin(9600);
+  Blynk.begin(Serial, auth);
+
+  // Outputs
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop()
+{
+  Blynk.run();
+}
