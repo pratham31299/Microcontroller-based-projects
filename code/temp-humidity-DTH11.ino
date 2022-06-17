011890255cf118453ffaec60e7fac70e36e2220e
Abhijeet kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2022-06-17 13:41:45 +0530

    Update temp-humidity-DTH11.ino

--- code/temp-humidity-DTH11.ino
+++ code/temp-humidity-DTH11.ino
@@ -1,4 +1,5 @@
-
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 // REQUIRES the following Arduino libraries:
 
@@ -15,20 +16,39 @@ int highT = 4; // above 35 deg celcius
 int lowH = 5;
 int highH = 6; // above 80 %
 
+LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE); // DHT sensor instence
 
 void setup() {
+
   Serial.begin(9600);
-
-  Serial.println(F("DHT11 test!"));
-
   dht.begin();
+  lcd.init();                      // initialize the lcd
+  lcd.init();
+
+  Serial.println(F("DHT11 begain !"));
+
+  // Print a message to the LCD.
+  lcd.backlight();
+  // Print message on LCD
+
+  lcd.setCursor(0, 0);
+  lcd.print("Initializing....");
+  delay(2000);
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("Temp/Humidity");
+  lcd.setCursor(4, 1);
+  lcd.print("Detector");
+  delay(2000);
+  lcd.clear();
+
+
+  // GPIO pins as OUTPUT
 
   pinMode(lowT, OUTPUT);
   pinMode(highT, OUTPUT);
-
   pinMode(lowH, OUTPUT);
   pinMode(highH, OUTPUT);
 
@@ -45,42 +65,49 @@ void loop() {
   float t = dht.readTemperature();
 
 
+  // Send data to debugging console
+
   Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
   Serial.println(F("°C "));
 
+  // Send data to LCD
+
+  lcd.setCursor(0, 0);
+  lcd.print("Temperature: ");
+  lcd.setCursor(13, 0);
+  lcd.print(t);
+  lcd.setCursor(0, 1);
+  lcd.print("Humidity % :");
+  lcd.setCursor(13, 1);
+  lcd.print(h);
+
+
+  // Control structurefor  OUTPUT Led's-----------------------
+
   if (t > 0) {
-
     digitalWrite(lowT, HIGH);
-
+    delay(200);
   }
-
   if (t >= 35) {
-
     digitalWrite(highT, HIGH);
-
+    delay(200);
   }
-
-
   if (h > 0) {
-
     digitalWrite(lowH, HIGH);
-
+    delay(200);
   }
-
   if (h >= 80) {
-
     digitalWrite(highH, HIGH);
+    delay(200);
 
   } else {
-
     digitalWrite(lowT, LOW);
     digitalWrite(lowH, LOW);
     digitalWrite(highH, LOW);
     digitalWrite(highT, LOW);
-
   }
 
 }
